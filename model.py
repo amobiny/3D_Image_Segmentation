4b7ad5a63a75160cd2ba55a96af3a2332d31e211
Aryan <aryan.mobiny@gmail.com>
2018-05-28 23:23:39 -0500

    ops added

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import os
+import tensorflow as tf
 
 
 class Unet_3D(object):
@@ -6,8 +7,6 @@ class Unet_3D(object):
     def __init__(self, sess, conf):
         self.sess = sess
         self.conf = conf
-        self.conv_size = (3, 3, 3)
-        self.pool_size = (2, 2, 2)
         self.axis, self.channel_axis = (1, 2, 3), 4
         self.input_shape = [self.conf.batch, self.conf.depth, self.conf.height, self.conf.width, self.conf.channel]
         self.output_shape = [self.conf.batch, self.conf.depth, self.conf.height, self.conf.width]
@@ -17,3 +16,14 @@ class Unet_3D(object):
             os.makedirs(conf.logdir)
         if not os.path.exists(conf.savedir):
             os.makedirs(conf.savedir)
+        self.create_placeholders()
+        self.build_network()
+
+    def create_placeholders(self):
+        with tf.name_scope('Input'):
+            self.x = tf.placeholder(tf.float32, self.input_shape, name='input')
+            self.y = tf.placeholder(tf.float32, self.output_shape, name='annotation')
+            self.keep_prob = tf.placeholder(tf.float32)
+
+    def inference(self):
+        self.predictions = self.inference(self.x)
---
+++ ops.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+
+
+def weight_variable(name, shape):
+    """
+    Create a weight variable with appropriate initialization
+    :param name: weight name
+    :param shape: weight shape
+    :return: initialized weight variable
+    """
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W_' + name, dtype=tf.float32,
+                           shape=shape, initializer=initer)
+
+
+def bias_variable(name, shape):
+    """
+    Create a bias variable with appropriate initialization
+    :param name: bias variable name
+    :param shape: bias variable shape
+    :return: initial bias variable
+    """
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b_' + name, dtype=tf.float32,
+                           initializer=initial)
+
+
+def conv_3d(inputs, filter_size, stride, num_filters, layer_name,
+            is_train=True, batch_norm=False, add_reg=False, use_relu=True):
+    """
+    Create a 2D convolution layer
+    :param inputs: input array
+    :param filter_size: size of the filter
+    :param stride: filter stride
+    :param num_filters: number of filters (or output feature maps)
+    :param layer_name: layer name
+    :param is_train: boolean to differentiate train and test (useful when applying batch normalization)
+    :param batch_norm: boolean to add the batch normalization layer (or not)
+    :param add_reg: boolean to add norm-2 regularization (or not)
+    :param use_relu: boolean to add ReLU non-linearity (or not)
+    :return: The output array
+    """
+    num_in_channel = inputs.get_shape().as_list()[-1]
+    with tf.variable_scope(layer_name):
+        shape = [filter_size, filter_size, filter_size, num_in_channel, num_filters]
+        weights = weight_variable(layer_name, shape=shape)
+        tf.summary.histogram('W', weights)
+        biases = bias_variable(layer_name, [num_filters])
+        layer = tf.nn.conv3d(input=inputs,
+                             filter=weights,
+                             strides=[1, stride, stride, stride, 1],
+                             padding="SAME")
+        print('{}: {}'.format(layer_name, layer.get_shape()))
+        if batch_norm:
+            layer = batch_norm_wrapper(layer, is_train)
+        layer += biases
+        if use_relu:
+            layer = tf.nn.relu(layer)
+        if add_reg:
+            tf.add_to_collection('weights', weights)
+    return layer
+
+
+def max_pool(x, ksize, stride, name):
+    """
+    Create a max pooling layer
+    :param x: input to max-pooling layer
+    :param ksize: size of the max-pooling filter
+    :param stride: stride of the max-pooling filter
+    :param name: layer name
+    :return: The output array
+    """
+    maxpool = tf.nn.max_pool3d(x,
+                               ksize=[1, ksize, ksize, ksize, 1],
+                               strides=[1, stride, stride, stride, 1],
+                               padding="SAME",
+                               name=name)
+    print('{}: {}'.format(maxpool.name, maxpool.get_shape()))
+    return maxpool
+
+
+def batch_norm_wrapper(inputs, is_training, decay=0.999, epsilon=1e-3):
+    """
+    creates a batch normalization layer
+    :param inputs: input array
+    :param is_training: boolean for differentiating train and test
+    :param decay:
+    :param epsilon:
+    :return: normalized input
+    """
+    scale = tf.Variable(tf.ones([inputs.get_shape()[-1]]))
+    beta = tf.Variable(tf.zeros([inputs.get_shape()[-1]]))
+    pop_mean = tf.Variable(tf.zeros([inputs.get_shape()[-1]]), trainable=False)
+    pop_var = tf.Variable(tf.ones([inputs.get_shape()[-1]]), trainable=False)
+
+    if is_training:
+        if len(inputs.get_shape().as_list()) == 5:  # For 3D convolutional layers
+            batch_mean, batch_var = tf.nn.moments(inputs, [0, 1, 2])
+        else:  # For fully-connected layers
+            batch_mean, batch_var = tf.nn.moments(inputs, [0])
+        train_mean = tf.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs, batch_mean, batch_var, beta, scale, epsilon)
+    else:
+        return tf.nn.batch_normalization(inputs, pop_mean, pop_var, beta, scale, epsilon)
